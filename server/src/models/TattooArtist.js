e7cfda6587a2afc291bd1eee1cc76f5ad0b05ef9
santicasalis <santicasalis@hotmail.com>
2023-11-21 15:28:00 -0300

    Deploy base de datos en raywall

--- server/src/models/TattooArtist.js
+++ server/src/models/TattooArtist.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "Customer",
+    "TattoArtist",
     {
       id: {
         type: DataTypes.UUID,
