bd294f7dfaf7b52d29d8e0f6c8a66fccc5a6040b
PGochman <pedrogoch55@gmail.com>
2023-11-23 18:56:21 -0300

    ruta 3000/api/upload para convertir imagen a url

--- server/src/handlers/imageHandlers/getImageHandler.js
+++
@@ -1,9 +0,0 @@
-const getImageController = require("../../controllers/imageControllers/getImageController")
-
-async function getImageHandler(req, res) {
-    const data = await req.formData()
-    
-    const response = await getImageController(data)
-}
-
-module.exports = getImageHandler
--- server/src/routes/imageRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const imageRouter = Router();
-const getImageHandler = require("../handlers/imageHandlers/getImageHandler")
-
-imageRouter.post("/", getImageHandler);
-
-module.exports = imageRouter
