e7f2dd895585aa2c70b4195e1d0dbb4117b0f304
Pedro Gochman <140557614+PGochman@users.noreply.github.com>
2023-11-24 09:24:15 -0300

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,14 +5,9 @@ const tattooArtistsRouter = require("./tattooArtistRouter");
 const customerRouter = require("./customerRouter")
 const publicationRouter = require("./publicationRouter")
 const tattooRouter = require("./tattooRouter");
-const tattooArtistRouter = require("./tattooArtistRouter");
 const tattoStyleRouter = require("./tattooStyleRouter");
 
-router.use("/tattooArtists", tattooArtistRouter);
 router.use("/tattooStyles", tattoStyleRouter);
-
-
-
 router.use("/tattooArtists", tattooArtistsRouter);
 router.use("/tattoo", tattooRouter);
 router.use("/publications", publicationRouter);
