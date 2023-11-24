05638f82513ec0dbc1ef768c5ed63d54ef89e9f9
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-24 16:30:49 -0300

    agregando "s" a las rutas

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,17 +2,17 @@ const { Router } = require("express");
 const router = Router();
 
 const tattooArtistsRouter = require("./tattooArtistRouter");
-const customerRouter = require("./customerRouter")
-const publicationRouter = require("./publicationRouter")
+const customerRouter = require("./customerRouter");
+const publicationRouter = require("./publicationRouter");
 const tattooRouter = require("./tattooRouter");
 const tattoStyleRouter = require("./tattooStyleRouter");
-const filterRouter = require("./filterRouter")
+const filterRouter = require("./filterRouter");
 
 router.use("/tattooStyles", tattoStyleRouter);
 router.use("/tattooArtists", tattooArtistsRouter);
-router.use("/tattoo", tattooRouter);
+router.use("/tattoos", tattooRouter);
 router.use("/publications", publicationRouter);
-router.use("/customer", customerRouter)
-router.use("/filter", filterRouter)
+router.use("/customers", customerRouter);
+router.use("/filters", filterRouter);
 
 module.exports = router;
