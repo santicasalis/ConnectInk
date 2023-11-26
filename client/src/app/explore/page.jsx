a6ffc1eb9ba1d70b0073a49dcd1ef6dd60725ebd
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2023-11-26 19:03:02 -0300

    explore page.jsx y FilterSideBar.jsx todo estilado a nuevo con flex y responsive para mantener las propiedades. No se toco nada de la funcionalidad, solo el estilado

--- client/src/app/explore/page.jsx
+++ client/src/app/explore/page.jsx
@@ -26,13 +26,13 @@ export default function ExplorePage() {
       <Nav />
 
       <div>
-        <section className="grid grid-cols-2 gap-x-1 text-center h-[300px] ">
-          <div className="col-span-1">
+        <section className="flex flex-col md:flex-row gap-x-1 text-center">
+          <div className="flex-1">
             <h2 className="font-bold h-[50%] text-4xl">
               Bienvenidos al Reino de la Inspiracion{" "}
               <span className="text-primary">Ink</span>orporada
             </h2>
-            <p className="text-justify h-[50%] ml-5 text-xl ">
+            <p className="text-justify h-[50%] ml-5 text-xl">
               En nuestro santuario del arte corporal, cada trazo cuenta una
               historia, cada línea lleva consigo la esencia de un viaje
               personal. En el lienzo de la piel, exploramos la intersección
@@ -41,7 +41,7 @@ export default function ExplorePage() {
             </p>
           </div>
 
-          <div className="col-span-1">
+          <div className="flex-1">
             <Swiper
               spaceBetween={25}
               parallax={true}
@@ -55,45 +55,42 @@ export default function ExplorePage() {
               }}
               modules={[Parallax, Autoplay, Pagination, Navigation]}
               className="w-[450px] flex justify-center items-center bg-secondary-100 h-[320px]
-                      bg-[url(https://vean-tattoo.es/media/images/Den_rozhd.2e16d0ba.fill-700x320-c0.format-jpeg.jpg)]
-                       bg-cover rounded   "
+                    bg-[url(https://vean-tattoo.es/media/images/Den_rozhd.2e16d0ba.fill-700x320-c0.format-jpeg.jpg)]
+                     bg-cover rounded"
             >
               <SwiperSlide className="mt-[150px] font-bold text-xl">
                 Encuentra la belleza en la simplicidad. Un tatuaje no solo
                 decora tu piel, sino que también cuenta tu historia en las
-                líneas más simples.
-              </SwiperSlide>
+                líneas más simples.{" "}
+              </SwiperSlide>{" "}
               <SwiperSlide className="mt-[150px] font-bold text-xl">
                 La poesía de la tinta. Cada tatuaje es una estrofa, cada estrofa
-                es una expresión. ¿Cuál será tu próximo verso?
-              </SwiperSlide>
+                es una expresión. ¿Cuál será tu próximo verso?{" "}
+              </SwiperSlide>{" "}
               <SwiperSlide className="mt-[150px] font-bold text-xl">
                 Palabras que perduran. Un tatuaje corto, una vida de
                 significado. Exprésate con precisión, deja que tu piel hable por
-                ti.
+                ti.{" "}
               </SwiperSlide>
+              //{" "}
               <SwiperSlide className="mt-[150px] font-bold text-xl">
                 Menos es más. Descubre la magia de las palabras concisas. Tus
-                tatuajes son recordatorios diarios de lo que realmente importa
+                tatuajes son recordatorios diarios de lo que realmente importa{" "}
               </SwiperSlide>
             </Swiper>
           </div>
         </section>
 
         <hr className="mt-[50px] border-primary ml-[50px] mr-[50px]"></hr>
-      </div>
 
-      
-
-        <div className="grid grid-cols-4 md:grid-cols-4 justify-center mt-10 mx-4 my-4 gap-x-4">
-
-          <div className="md:col-span-1 ">
+        <div className="flex flex-col md:flex-row justify-center mt-10 mx-4 my-4 gap-x-4">
+          <div className="md:flex-1">
             <FilterSideBar />
           </div>
 
-          <div className="md:col-span-3 grid grid-cols-2 gap-x-2">
+          <div className="md:flex-[3] flex flex-wrap gap-x-2">
             {filtered?.map((filter) => (
-              <div key={filter.id} className="mb-4">
+              <div key={filter.id} className="mb-4 w-full md:w-1/2">
                 <Card
                   key={filter.id}
                   name={filter.name}
@@ -108,5 +105,97 @@ export default function ExplorePage() {
           </div>
         </div>
       </div>
+    </div>
   );
 }
+
+
+//   return (
+//     <div className="w-full">
+//       <Nav />
+
+//       <div>
+//         <section className="grid grid-cols-2 gap-x-1 text-center h-[300px] ">
+//           <div className="col-span-1">
+//             <h2 className="font-bold h-[50%] text-4xl">
+//               Bienvenidos al Reino de la Inspiracion{" "}
+//               <span className="text-primary">Ink</span>orporada
+//             </h2>
+//             <p className="text-justify h-[50%] ml-5 text-xl ">
+//               En nuestro santuario del arte corporal, cada trazo cuenta una
+//               historia, cada línea lleva consigo la esencia de un viaje
+//               personal. En el lienzo de la piel, exploramos la intersección
+//               entre la imaginación y la realidad, transformando ideas en
+//               tatuajes que resuenan con significado.
+//             </p>
+//           </div>
+
+//           <div className="col-span-1">
+//             <Swiper
+//               spaceBetween={25}
+//               parallax={true}
+//               centeredSlides={true}
+//               autoplay={{
+//                 delay: 3500,
+//                 disableOnInteraction: false,
+//               }}
+//               pagination={{
+//                 clickable: true,
+//               }}
+//               modules={[Parallax, Autoplay, Pagination, Navigation]}
+//               className="w-[450px] flex justify-center items-center bg-secondary-100 h-[320px]
+//                       bg-[url(https://vean-tattoo.es/media/images/Den_rozhd.2e16d0ba.fill-700x320-c0.format-jpeg.jpg)]
+//                        bg-cover rounded   "
+//             >
+//               <SwiperSlide className="mt-[150px] font-bold text-xl">
+//                 Encuentra la belleza en la simplicidad. Un tatuaje no solo
+//                 decora tu piel, sino que también cuenta tu historia en las
+//                 líneas más simples.
+//               </SwiperSlide>
+//               <SwiperSlide className="mt-[150px] font-bold text-xl">
+//                 La poesía de la tinta. Cada tatuaje es una estrofa, cada estrofa
+//                 es una expresión. ¿Cuál será tu próximo verso?
+//               </SwiperSlide>
+//               <SwiperSlide className="mt-[150px] font-bold text-xl">
+//                 Palabras que perduran. Un tatuaje corto, una vida de
+//                 significado. Exprésate con precisión, deja que tu piel hable por
+//                 ti.
+//               </SwiperSlide>
+//               <SwiperSlide className="mt-[150px] font-bold text-xl">
+//                 Menos es más. Descubre la magia de las palabras concisas. Tus
+//                 tatuajes son recordatorios diarios de lo que realmente importa
+//               </SwiperSlide>
+//             </Swiper>
+//           </div>
+//         </section>
+
+//         <hr className="mt-[50px] border-primary ml-[50px] mr-[50px]"></hr>
+//       </div>
+
+      
+
+//         <div className="grid grid-cols-4 md:grid-cols-4 justify-center mt-10 mx-4 my-4 gap-x-4">
+
+//           <div className="md:col-span-1 ">
+//             <FilterSideBar />
+//           </div>
+
+//           <div className="md:col-span-3 grid grid-cols-2 gap-x-2">
+//             {filtered?.map((filter) => (
+//               <div key={filter.id} className="mb-4">
+//                 <Card
+//                   key={filter.id}
+//                   name={filter.name}
+//                   lastName={filter.lastName}
+//                   location={filter.location}
+//                   shopName={filter.shopName}
+//                   tattoos={filter.Publications}
+//                   image={filter.image}
+//                 />
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//   );
+
