8bfc97f6a020d82b19da7b687893a6d32a89a577
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-26 11:43:34 -0300

    fix try-catch

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 const PORT = 3001;
 
 conn
-  .sync({ alter: true })
+  .sync({ alter: false })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
