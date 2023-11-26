74e3822ffce4cbf31ba3d544a6602449c9e3c994
facupadilla <facupadilla1904@gmail.com>
2023-11-25 22:47:45 -0300

    diseño de explore y filt

--- client/src/app/redux/features/artists/artistsSlice.js
+++ client/src/app/redux/features/artists/artistsSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     people: [],
-    
+    filtered:[]
 }
 
 export const artistsSlice = createSlice({
@@ -13,13 +13,17 @@ export const artistsSlice = createSlice({
     reducers: {
         getArtists: (state, action) => {
             state.people = action.payload
+            state.filtered = action.payload
         },
-       
+        filterArtist:(state,action)=>{
+            state.filtered = action.payload
+        }
 
     }
 })
 
-export const {getArtists} = artistsSlice.actions
+export const {getArtists, filterArtist} = artistsSlice.actions
+
 
 
 export default artistsSlice.reducer
