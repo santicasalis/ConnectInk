87cdcb2a4b51005c3b930e29a59a5a964b9384ad
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2023-11-26 16:49:45 -0300

    cambios de estilo en el log-in

--- client/src/app/auth/page.jsx
+++ client/src/app/auth/page.jsx
@@ -9,6 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserId } from '../redux/features/user/userActions';
 
+
 const Login = () => {
   const artists = useSelector((state) => state.artists.people)
   const dispatch = useDispatch()
@@ -43,42 +44,83 @@ const Login = () => {
     });
   }
   return (
-    <div className="bg-secondary-100 p-8 rounded-xl shadow-xl w-auto lg:w-[450px]">
+    <div className=" div-img relative rounded-xl shadow-xl w-full xl:w-1/3 lg:w-1/4 md:w-1/2">
+
+      <div className='bg-black absolute opacity-75 w-full h-full '>
+        <img className='object-cover' src=" /dsgnlogin4.png " alt="" />
+      </div>
+
+
       <h1 className="text-3xl text-center font-bold tracking-[3px] text-white mb-8">
         Ingresar
       </h1>
-      <form onSubmit={handleSubmit} className='mb-7'>
-          <button className='flex items-center justify-center gap-4 mb-8 bg-secondary-900 w-full py-3 px-4 rounded-xl text-gray-100'>
-            <img src='https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png' className="w-4 h-4"/>
-            Ingresar con Google
+      <form onSubmit={handleSubmit} className="mb-7">
+        <button className="flex items-center justify-center gap-4 mb-8 bg-secondary-900 w-full py-3 px-4 rounded-xl text-gray-100">
+          <img
+            src="https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png"
+            className="w-4 h-4"
+          />
+          Ingresar con Google
+        </button>
+        <div className="relative mb-3">
+          <RiMailLine className="absolute top-1/2 -translate-y-1/2 left-2 text-primary" />
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            className="py-3 pl-8 pr-4 bg-secondary-900 w-full outline-none rounded-lg"
+            placeholder="E-mail"
+          />
+        </div>
+        <div className="relative mb-4">
+          <RiLockLine className="absolute top-1/2 -translate-y-1/2 left-2 text-primary" />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            onChange={handleChange}
+            className="py-3 px-8 bg-secondary-900 w-full outline-none rounded-lg"
+            placeholder="Contraseña"
+          />
+          {showPassword ? (
+            <RiEyeOffLine
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary"
+            />
+          ) : (
+            <RiEyeLine
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary"
+            />
+          )}
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="bg-black opacity-50 text-white uppercase font-bold text-sm w-full py-3 px-4 rounded-lg hover:bg-primary/90 transition-colors"
+          >
+            Ingresar
           </button>
-          <div className='relative mb-3'>
-            <RiMailLine className='absolute top-1/2 -translate-y-1/2 left-2 text-primary'/>
-            <input type='email' name='email' onChange={handleChange} className='py-3 pl-8 pr-4 bg-secondary-900 w-full outline-none rounded-lg' placeholder='E-mail'/>
-          </div>
-          <div className='relative mb-4'>
-            <RiLockLine className='absolute top-1/2 -translate-y-1/2 left-2 text-primary'/>
-            <input type={showPassword ? "text" : "password"} name='password' onChange={handleChange} className='py-3 px-8 bg-secondary-900 w-full outline-none rounded-lg' placeholder='Contraseña'/>
-            {
-              showPassword
-              ? (<RiEyeOffLine onClick={()=>setShowPassword(!showPassword)} className='absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary'/>)
-              : (<RiEyeLine onClick={()=>setShowPassword(!showPassword)} className='absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary'/>)
-            }
-          </div>
-          <div>
-            <button type='submit'className='bg-primary text-black uppercase font-bold text-sm w-full py-3 px-4 rounded-lg hover:bg-primary/90 transition-colors'>
-              Ingresar
-            </button>
-          </div>
+        </div>
       </form>
-      <div className='flex flex-col gap-3 items-center'>
-          <Link href='/auth/forgot-password' className='text-gray-200 hover:text-primary transition-colors'>¿Olvidaste tu contraseña?</Link>
-          <span className='flex items-center gap-2'>
-              Si no tienes una cuenta <Link href='/auth/register' className='text-primary hover:text-gray-100 transition-colors'>Registrate</Link> 
-          </span>   
+      <div className="flex flex-col gap-3 items-center">
+        <Link
+          href="/auth/forgot-password"
+          className="text-gray-200 hover:text-primary transition-colors"
+        >
+          ¿Olvidaste tu contraseña?
+        </Link>
+        <span className="flex items-center gap-2">
+          Si no tienes una cuenta{" "}
+          <Link
+            href="/auth/register"
+            className="text-primary hover:text-gray-100 transition-colors"
+          >
+            Registrate
+          </Link>
+        </span>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login
