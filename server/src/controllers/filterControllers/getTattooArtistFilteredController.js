fc6875ca6eca4f07db1c10afc8ca6e8028609aac
PGochman <pedrogoch55@gmail.com>
2023-11-25 21:34:11 -0300

    arreglado filtro con el front

--- server/src/controllers/filterControllers/getTattooArtistFilteredController.js
+++ server/src/controllers/filterControllers/getTattooArtistFilteredController.js
@@ -1,27 +1,51 @@
 const { TattooArtist, TattooStyle } = require('../../db')
 const { Op } = require('sequelize')
 
-    const getTattooArtistFiltered = async (location, styles) => {
-        const tattooArtistsFound = await TattooArtist.findAll({
-            where: {
-                location: {
-                    [Op.iLike]: `%${location}%`
-                }
-            },
-            include: [
-                {
-                    model: TattooStyle,
+    const getTattooArtistFiltered = async (location , styles) => {
+        if(styles.length > 0) {
+            try {
+
+                const tattooArtistsFound = await TattooArtist.findAll({
                     where: {
-                        name: {
-                            [Op.in]: styles
+                        location: {
+                            [Op.iLike]: `%${location}%`
                         }
+                    },
+                    include: [
+                        {
+                            model: TattooStyle,
+                            where: {
+                                name: {
+                                    [Op.in]: styles
+                                }
+                            }
+                        }
+                    ]
+                });
+
+                return tattooArtistsFound
+            } catch (error) {
+                console.log(error)
+            }
+
+        } else  {
+            try {
+
+            const tattooArtistsFound = await TattooArtist.findAll({
+                where: {
+                    location: {
+                        [Op.iLike]: `%${location}%`
                     }
-                }
-            ]
-        });
-    return tattooArtistsFound
+                },
+
+            });
+
+            return tattooArtistsFound
+        } catch (error) {
+            console.log(error)
+        } 
+    }
 
 }
 
 module.exports = getTattooArtistFiltered
-
