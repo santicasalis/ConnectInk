ae91129a6d59c475ff62ab3f5b92485b2954e08c
santicasalis <santicasalis@hotmail.com>
2023-11-27 20:20:16 -0300

    Se realiza log-in con google y se obtienen los datos

--- client/src/app/a-dashboard/home/page.jsx
+++ client/src/app/a-dashboard/home/page.jsx
@@ -1,13 +1,33 @@
-import TopBarOptions from '@/components/topBarOptions/TopBarOptions'
-import React from 'react'
+"use client";
+import TopBarOptions from "@/components/topBarOptions/TopBarOptions";
+import React, { useEffect } from "react";
+import { auth } from "../../../firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = () => {
-  
-  return (
-    <div>
-      Inicio
-    </div>
-  )
-}
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const userInfo = {
+          uid: user.uid,
+          nombreCompleto: user.displayName,
+          image: user.photoURL,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          provider: user.providerId,
+        };
 
-export default Home
+        console.log(userInfo);
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
+
+  return <div>Inicio</div>;
+};
+
+export default Home;
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,81 +1,140 @@
-"use client"
-import React from 'react'
-import {RiNotification3Line,RiArrowDownSLine, RiSettings5Fill,RiLogoutCircleRLine, RiReplyLine} from "react-icons/ri";
-import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css'
-import '@szhsin/react-menu/dist/transitions/slide.css'
-import Link from 'next/link'
-import { useDispatch, useSelector } from 'react-redux';
-import Image from 'next/image';
-import { logOut } from '@/app/redux/features/user/userActions';
-import {useRouter} from 'next/navigation';
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  RiNotification3Line,
+  RiArrowDownSLine,
+  RiSettings5Fill,
+  RiLogoutCircleRLine,
+  RiReplyLine,
+} from "react-icons/ri";
+import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import Image from "next/image";
+import { logOut } from "@/app/redux/features/user/userActions";
+import { useRouter } from "next/navigation";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
+
+import { onAuthStateChanged } from "firebase/auth";
 
 const Header = () => {
+  const user = useSelector((state) => state.user);
+  const [userInfo, setUserInfo] = useState(null);
 
-  const user = useSelector((state) => state.user)
-  const imageLoader = ({src}) => {
-    return src
-  }
-  const dispatch = useDispatch()
-  const router = useRouter()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userInformation = {
+          uid: user.uid,
+          nombreCompleto: user.displayName,
+          image: user.photoURL,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          provider: user.providerId,
+        };
+
+        setUserInfo(userInformation);
+      } else {
+        // No hay usuario autenticado
+        setUserInfo(null);
+      }
+    });
+
+    // Desvincula el listener cuando el componente se desmonta
+  }, [auth]);
+
+  console.log(userInfo);
+  const imageLoader = ({ src }) => {
+    return src;
+  };
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const handleLogOut = () => {
-    dispatch(logOut())
-    router.replace("/")
-  }
-  
-  return (
-    <header className=' bg-secondary-900 h-[8vh] md:h-[8vh] border-b border-gray-600 p-8 flex items-center justify-end'>
-        <nav className='w-full flex items-center gap-x-4 justify-between'>
-            <Link href='/' className='flex items-center gap-x-1 hover:bg-secondary-100 rounded-lg p-2'>
-                <RiReplyLine className=''/>
-                Volver al inicio
-            </Link>
-            <div className='flex items-center gap-x-4'>
-              <button className='relative hover:bg-secondary-100 p-2 rounded-lg transition-colors'>
-                  <RiNotification3Line/>
-                  <span className='absolute -top-0 -right-0 bg-red-600 py-0.5 px-1 text-white rounded-full text-[8px] font-bold'>
-                      2
-                  </span>
-              </button>
-              
-              <Menu menuButton={<MenuButton className='flex items-center gap-x-2 hover:bg-secondary-100 py-2 px-4 rounded-lg'>
-                <Image src={user.image} loader={imageLoader} width={40} height={40} alt={`${user.name} ${user.lastName} profile pic`} />
-                  <span>
-                      {`${user.name} ${user.lastName}`}
-                  </span>
-                  <RiArrowDownSLine />
-              </MenuButton>}
-              menuStyle={{backgroundColor:'#252524', color:'white', padding:3}}
-              transition
-              >
-                <MenuItem className="rounded-lg transition-colors border-b-2 border-gray-500/50">
-                    <Link href='/a-dashboard/profile' className='flex items-center gap-x-4'>
-                      <Image src={user.image} loader={imageLoader} width={40} height={40} alt={`${user.name} ${user.lastName} profile pic`} />  
-                        <div className='flex flex-col gap-1 text-sm'>
-                          <span>{`${user.name} ${user.lastName}`}</span>
-                          <span className='text-[9px]'>{user.email}</span>
-                        </div>
-                    </Link>
-                </MenuItem>
-                <MenuItem>
-                    <Link href='' className='flex items-center gap-2 text-sm py-1.5'>
-                        <RiSettings5Fill />
-                        Settings
-                    </Link>
-                </MenuItem>
-                <MenuItem>
-                        <button onClick={handleLogOut}>
-                          <RiLogoutCircleRLine />
-                          Logout
-                        </button>
-                </MenuItem>
-              </Menu>
-            </div>
-          
-        </nav>
-    </header>
-  )
-}
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        alert("sign out ok");
+      })
+      .catch((error) => {
+        // An error happened.
+        throw error;
+      });
+    dispatch(logOut());
+    router.replace("/");
+  };
 
-export default Header
+  return (
+    <header className=" bg-secondary-900 h-[8vh] md:h-[8vh] border-b border-gray-600 p-8 flex items-center justify-end">
+      <nav className="w-full flex items-center gap-x-4 justify-between">
+        <Link
+          href="/"
+          className="flex items-center gap-x-1 hover:bg-secondary-100 rounded-lg p-2"
+        >
+          <RiReplyLine className="" />
+          Volver al inicio
+        </Link>
+        <div className="flex items-center gap-x-4">
+          <button className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+            <RiNotification3Line />
+            <span className="absolute -top-0 -right-0 bg-red-600 py-0.5 px-1 text-white rounded-full text-[8px] font-bold">
+              2
+            </span>
+          </button>
+
+          <Menu
+            menuButton={
+              <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-100 py-2 px-4 rounded-lg">
+                {/* <Image src={user.image} loader={imageLoader} width={40} height={40} alt={`${user.name} ${user.lastName} profile pic`} /> */}
+                <span>{`${userInfo?.nombreCompleto}`}</span>
+                <RiArrowDownSLine />
+              </MenuButton>
+            }
+            menuStyle={{
+              backgroundColor: "#252524",
+              color: "white",
+              padding: 3,
+            }}
+            transition
+          >
+            <MenuItem className="rounded-lg transition-colors border-b-2 border-gray-500/50">
+              <Link
+                href="/a-dashboard/profile"
+                className="flex items-center gap-x-4"
+              >
+                <Image
+                  src={userInfo?.image}
+                  loader={imageLoader}
+                  width={40}
+                  height={40}
+                  alt={`${userInfo?.nombreCompleto}  profile pic`}
+                />
+                <div className="flex flex-col gap-1 text-sm">
+                  <span>{`${userInfo?.nombreCompleto}`}</span>
+                  <span className="text-[9px]">{userInfo?.email}</span>
+                </div>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="" className="flex items-center gap-2 text-sm py-1.5">
+                <RiSettings5Fill />
+                Settings
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <button onClick={handleLogOut}>
+                <RiLogoutCircleRLine />
+                Logout
+              </button>
+            </MenuItem>
+          </Menu>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
