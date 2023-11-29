4dc808dd4a6606aa79f5d477c25575a2689ea271
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2023-11-29 19:42:42 -0300

    copiado de la rama de Pedro feature/create-appointment

--- client/src/app/explore/[id]/booking/page.jsx
+++ client/src/app/explore/[id]/booking/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
@@ -6,22 +6,30 @@ import { useState } from "react";
 import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 import moment from "moment";
+import axios from "axios";
 
+const URL_BASE = "http://localhost:3001";
 
 const bookAppointment = () => {
   //hardcodeo horarios
   const horarios = {
-    turno1: "13hs",
-    turno2: "15hs",
-    turno3: "17hs",
-    turno4: "19hs",
-    turno5: "21hs",
+    turno1: "13",
+    turno2: "15",
+    turno3: "17",
+    turno4: "19",
+    turno5: "21",
   };
 
-
   const [horarioNoDisponible, setHorarioNoDisponible] = useState(null);
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
   const actualizarFecha = (fecha) => {
+    const date = fecha._d;
+    setDay(date.getDate());
+    setMonth(date.getMonth());
+    setYear(date.getYear() + 1900);
     setFechaSeleccionada(fecha);
     setHorarioSeleccionado(null);
     //seleccionar un horario al azar como no disponible
@@ -39,8 +47,6 @@ const bookAppointment = () => {
     setHorarioSeleccionado(horario);
   };
 
-
-
   const [fechaSeleccionada, setFechaSeleccionada] = useState(null);
   const [horarioSeleccionado, setHorarioSeleccionado] = useState(null);
 
@@ -53,6 +59,15 @@ const bookAppointment = () => {
   // const seleccionarHorario = (horario) => {
   //   setHorarioSeleccionado(horario);
   // };
+
+  const reservarTurno = async () => {
+    const date = new Date(year, month, day, horarioSeleccionado);
+
+    const response = await axios.post(`${URL_BASE}/appointments`, {
+      dateAndTime: date,
+    });
+    console.log(response);
+  };
 
   return (
     <div className="w-full md:w-2/3 p-4 shadow-lg">
@@ -88,14 +103,113 @@ const bookAppointment = () => {
                 }`}
                 onClick={() => seleccionarHorario(horario)}
               >
-                {horario}
+                {`${horario}hs`}
               </button>
             ))}
           </div>
         )}
       </div>
+      <button onClick={reservarTurno}>Reservar turno</button>
     </div>
   );
-}
+};
 
-export default bookAppointment
+export default bookAppointment;
+
+// 'use client'
+
+// import { useState } from "react";
+
+// //HOOKS DEL CALENDARIO
+// import Datetime from "react-datetime";
+// import "react-datetime/css/react-datetime.css";
+// import moment from "moment";
+
+// const bookAppointment = () => {
+//   //hardcodeo horarios
+//   const horarios = {
+//     turno1: "13hs",
+//     turno2: "15hs",
+//     turno3: "17hs",
+//     turno4: "19hs",
+//     turno5: "21hs",
+//   };
+
+//   const [horarioNoDisponible, setHorarioNoDisponible] = useState(null);
+
+//   const actualizarFecha = (fecha) => {
+//     setFechaSeleccionada(fecha);
+//     setHorarioSeleccionado(null);
+//     //seleccionar un horario al azar como no disponible
+//     const keys = Object.keys(horarios);
+//     const randomKey = keys[Math.floor(Math.random() * keys.length)];
+//     setHorarioNoDisponible(horarios[randomKey]);
+//   };
+
+//   const seleccionarHorario = (horario) => {
+//     if (horario === horarioNoDisponible) {
+//       //seleccionar un horario no disponible
+//       alert("Este horario no está disponible");
+//       return;
+//     }
+//     setHorarioSeleccionado(horario);
+//   };
+
+//   const [fechaSeleccionada, setFechaSeleccionada] = useState(null);
+//   const [horarioSeleccionado, setHorarioSeleccionado] = useState(null);
+
+//   //DESCOMENTAR CUANDO YA TENGAMOS LA CONEXION CON LA DB
+//   // const actualizarFecha = (fecha) => {
+//   //   setFechaSeleccionada(fecha);
+//   //   setHorarioSeleccionado(null);
+//   // };
+
+//   // const seleccionarHorario = (horario) => {
+//   //   setHorarioSeleccionado(horario);
+//   // };
+
+//   return (
+//     <div className="w-full md:w-2/3 p-4 shadow-lg">
+//       <div className="p-4 rounded border-primary border-[2px] shadow-lg">
+//         <h3 className="text-xl font-bold mb-2">Reserve su turno</h3>
+//         <Datetime
+//           value={fechaSeleccionada}
+//           onChange={actualizarFecha}
+//           timeFormat={false}
+//           open={true}
+//           input={false}
+//         />
+//         <button
+//           type="button"
+//           className="bg-primary text-white font-bold py-2 px-4 rounded mt-4"
+//         >
+//           Reservar
+//         </button>
+
+//         {/* Horarios disponibles después de seleccionar una fecha. Esta logica va a tener que revisarse cuando tengamos ya la conexion con la DB */}
+//         {fechaSeleccionada && (
+//           <div className="mt-4">
+//             <h4 className="text-lg font-bold mb-2">Horarios Disponibles</h4>
+//             {Object.values(horarios).map((horario, index) => (
+//               <button
+//                 key={index}
+//                 className={`block mb-2 px-4 py-2 rounded ${
+//                   horario === horarioSeleccionado
+//                     ? "bg-blue-500 text-white"
+//                     : horario === horarioNoDisponible
+//                     ? "bg-red-500 text-white"
+//                     : "bg-white text-black"
+//                 }`}
+//                 onClick={() => seleccionarHorario(horario)}
+//               >
+//                 {horario}
+//               </button>
+//             ))}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default bookAppointment
