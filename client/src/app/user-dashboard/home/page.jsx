7fe8be33d048edd947d70528ef2220e6642ae7cf
Constanzamw <cweiner92@gmail.com>
2023-11-29 20:49:28 -0300

    user dashboard

---
+++ client/src/app/user-dashboard/home/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+import TopBarOptions from "@/components/topBarOptions/TopBarOptions";
+import React, { useEffect } from "react";
+import { auth } from "../../../firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
+import PostsDashboard from '@/components/postsDashboard/PostsDashboard'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <PostsDashboard />
+    </div>
+  )
+}
+
+export default Home;
