7ba933ad457ba30c1a26afd675a72d4822b11b0b
Constanzamw <cweiner92@gmail.com>
2023-11-30 11:21:54 -0300

    admin dash add/remove estilos

--- client/src/app/redux/features/styles/stylesActions.js
+++ client/src/app/redux/features/styles/stylesActions.js
@@ -1,4 +1,4 @@
-import { getStyles } from "./stylesSlice";
+import { getStyles,addStyle,deleteStyle } from "./stylesSlice";
 import axios from "axios";
 
 const URL_BASE = "http://localhost:3001"
@@ -9,3 +9,12 @@ export const getAllStyles = () => async (dispatch) =>{
     dispatch(getStyles(allStyles))
 }
 
+export const addNewStyle = (newStyle) => async (dispatch) => {
+    const addedStyle = (await axios.post(`${URL_BASE}/tattooStyles`, newStyle)).data;
+    dispatch(addStyle(addedStyle));
+  };
+
+  export const removeStyle = (styleId) => async (dispatch) => {
+    await axios.delete(`${URL_BASE}/tattooStyles/${styleId}`);
+    dispatch(deleteStyle(styleId));
+  };
