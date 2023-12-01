0351079f4a4516e44e3cb0740a0be5c232358632
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2023-12-01 11:13:31 -0300

    rama presentacion

--- client/src/app/auth/layout.jsx
+++ client/src/app/auth/layout.jsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }) {
   return (
       <div className='relative max-h-screen w-full flex items-center justify-center  overflow-hidden'>
         <img src='https://images3.alphacoders.com/866/866852.jpg' className="w-full h-full  object-cover"/>
-        <div className="absolute top-0 left-0 w-full h-full custom-gradient-login"></div>
+        {/* <div className="absolute top-0 left-0 w-full h-full custom-gradient-login"></div> */}
         {children} 
       </div>
   )
