d9953932b55a6cfb5b15a612c688f34f19c4f1a3
alejoguzmann <alejoguzmann20@gmail.com>
2023-12-02 10:36:21 -0300

    clientes con turnos incluidos

--- server/src/controllers/customerControllers/getCustomerById.js
+++ server/src/controllers/customerControllers/getCustomerById.js
@@ -1,14 +1,24 @@
-const { Customer } = require('../../db')
+const { Customer, CustomerTattooArtistAppointment, Appointment } = require('../../db')
 
 const getCustomerById = async (id) => {
     const customer = await Customer.findByPk(id)
+    const appointmentsByArtist = await CustomerTattooArtistAppointment.findAll({
+        where: { CustomerId: id },
+        include: [
+            {
+                model: Appointment,
+            },
+        ],
+    });
     return {
         tokenId: customer.tokenId,
         fullName: customer.fullName,
         lastName: customer.lastName,
         email: customer.email,
         phone: customer.phone,
-        image: customer.image
+        image: customer.image,
+        appointments: appointmentsByArtist?.map(appointment => appointment.Appointment)
+
     }
 }
 module.exports = getCustomerById
