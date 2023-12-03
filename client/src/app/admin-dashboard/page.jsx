84878e089c430710006c2d87a89e9f8bde363519
facupadilla <facupadilla1904@gmail.com>
2023-12-03 17:52:08 -0300

    arreglos en general

--- client/src/app/admin-dashboard/page.jsx
+++ client/src/app/admin-dashboard/page.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import {RiEdit2Line} from "react-icons/ri"
+import { TbPointFilled } from "react-icons/tb";
 
 const AdminDashboard = () => {
   
   return (
     <div className='bg-secondary-900 p-8 rounded-xl w-full'>
-      <h1 className='text-4xl'> Admin </h1>
+      <h1 className='text-4xl'> ¡Bienvenido al Panel de Control Administrativo! </h1>
       <hr className='my-8 border-gray-500'/>
-    </div>
+    <ul className='text-[17px]'>
+      <li className='flex gap-2'><TbPointFilled className='text-primary'/>Tu plataforma central para gestionar todas las actividades administrativas de nuestra página de tatuajes.</li>
+      <li className='flex gap-2'><TbPointFilled className='text-primary' />Gracias por contribuir al éxito y la profesionalidad de nuestra comunidad.</li>
+      <li className='flex gap-2'><TbPointFilled className='text-primary' />Explora las herramientas y funciones disponibles para optimizar la gestión de contenido.</li>
+    </ul>
+  </div>
   )
 }
 
