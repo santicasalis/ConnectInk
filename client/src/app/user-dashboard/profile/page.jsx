d42ed5577baaf388376d65684b3707070268a812
Constanzamw <cweiner92@gmail.com>
2023-12-03 21:49:04 -0300

    arreglo codigo profile

--- client/src/app/user-dashboard/profile/page.jsx
+++ client/src/app/user-dashboard/profile/page.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import {  RiEyeLine,  RiEyeOffLine,} from "react-icons/ri";
 import { useState } from 'react'
 
-const Profile = () => {
+const UProfile = () => {
 
   const user = useSelector((state) => state.user.logedInUser)
   const [showPassword, setShowPassword] = useState(false);
@@ -51,7 +51,7 @@ const Profile = () => {
           </div>
         </div>
         
-        <div className='flex items-center mb-4'>
+        {/* <div className='flex items-center mb-4'>
           <div className='w-1/4'>
               <p>Telefono: <span className='text-red-500'>*</span></p>
           </div>
@@ -60,7 +60,7 @@ const Profile = () => {
                 <input type="text" value={user.phone} className='w-full py-3 px-4 outline-none rounded-lg bg-secondary-900 cursor-default' readOnly/>
               </div>
           </div>
-        </div>
+        </div> */}
         <div className='flex items-center mb-4'>
           <div className='w-1/4'>
               <p>Email: <span className='text-red-500'>*</span></p>
@@ -71,34 +71,14 @@ const Profile = () => {
               </div>
           </div>
         </div>
-        <div className='flex items-center mb-4'>
-          <div className='w-1/4'>
-              <p>Constraseña: <span className='text-red-500'>*</span></p>
-          </div>
-          <div className='flex-1 flex items-center gap-4'>
-              <div className='w-1/2 relative'>
-                <input type={showPassword ? "text" : "password"} value={user.password} className='w-full py-3 px-4 outline-none rounded-lg bg-secondary-900 cursor-default' readOnly/>
-                {showPassword ? (
-            <RiEyeOffLine
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary"
-            />
-          ) : (
-            <RiEyeLine
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute top-1/2 -translate-y-1/2 right-2 hover:cursor-pointer text-primary"
-            />
-          )}
-              </div>
-          </div>
-        </div>  
+        
         
       </form>
     </div>
   )
 }
 
-// export default Profile
+export default UProfile
 
 // const Profile = () => {
 //   const user = useSelector((state) => state.user.logedInUser)
