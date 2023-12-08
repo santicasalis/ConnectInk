09b75d114f35d3755228d7c5623278173e8118da
alejoguzmann <alejoguzmann20@gmail.com>
2023-12-08 11:05:10 -0300

    solucion posts

--- client/src/app/a-dashboard/home/page.jsx
+++ client/src/app/a-dashboard/home/page.jsx
@@ -3,7 +3,7 @@ import TopBarOptions from "../../../components/topBarOptions/TopBarOptions";
 import React, { useEffect } from "react";
 import { auth } from "../../../firebase.js";
 import { onAuthStateChanged } from "firebase/auth";
-import PostsDashboard from '../../../components/postDashboard/PostDashboard'
+import PostsDashboard from '../../../components/postsDashboard/PostsDashboard'
 
 
 const Home = () => {
