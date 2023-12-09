bc1991a5ddd32048380e25d34dbef14ea6463f4d
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2023-12-09 17:02:54 -0300

    se puede editar perfil de cliente y artista

--- server/src/controllers/tattooArtistControllers/updateTattooArtist.js
+++ server/src/controllers/tattooArtistControllers/updateTattooArtist.js
@@ -1,5 +1,47 @@
+// const { TattooArtist } = require("../../db");
+// const crypto = require("crypto");
+// const updateTattooArtist = async (
+//   id,
+//   fullName,
+//   email,
+//   password,
+//   phone,
+//   instagram,
+//   description,
+//   address,
+//   location,
+//   shopName,
+//   image
+// ) => {
+//   const tattooArtistFound = await TattooArtist.findByPk(id);
+//   if (tattooArtistFound) {
+//     await TattooArtist.update(
+//       {
+//         fullName: fullName,
+//         email: email,
+//         password: crypto.createHash("sha256").update(password).digest("hex"),
+//         phone: phone,
+//         instagram: instagram,
+//         description: description,
+//         address: address,
+//         location: location,
+//         shopName: shopName,
+//         image: image,
+//       },
+//       { where: { id: id } }
+//     );
+//     return "Profile updated successfully";
+//   } else {
+//     return "Not found";
+//   }
+// };
+
+// module.exports = updateTattooArtist;
+
+
 const { TattooArtist } = require("../../db");
 const crypto = require("crypto");
+
 const updateTattooArtist = async (
   id,
   fullName,
@@ -15,21 +57,23 @@ const updateTattooArtist = async (
 ) => {
   const tattooArtistFound = await TattooArtist.findByPk(id);
   if (tattooArtistFound) {
-    await TattooArtist.update(
-      {
-        fullName: fullName,
-        email: email,
-        password: crypto.createHash("sha256").update(password).digest("hex"),
-        phone: phone,
-        instagram: instagram,
-        description: description,
-        address: address,
-        location: location,
-        shopName: shopName,
-        image: image,
-      },
-      { where: { id: id } }
-    );
+    const updateData = {};
+    if (fullName) updateData.fullName = fullName;
+    if (email) updateData.email = email;
+    if (password)
+      updateData.password = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
+    if (phone) updateData.phone = phone;
+    if (instagram) updateData.instagram = instagram;
+    if (description) updateData.description = description;
+    if (address) updateData.address = address;
+    if (location) updateData.location = location;
+    if (shopName) updateData.shopName = shopName;
+    if (image) updateData.image = image;
+
+    await TattooArtist.update(updateData, { where: { id: id } });
     return "Profile updated successfully";
   } else {
     return "Not found";
