55703d7735c943cb858047136f5837497a4d7d42
PGochman <pedrogoch55@gmail.com>
2023-12-09 16:40:19 -0300

    reviews en detail de artista

--- client/src/app/explore/[id]/page.jsx
+++ client/src/app/explore/[id]/page.jsx
@@ -1,39 +1,50 @@
-'use client'
+"use client";
 
 // Importaciones
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Nav from "@/components/nav/Nav";
-import "./style.css"; 
-import MapComponent from "@/components/map/Map"
+import "./style.css";
+import MapComponent from "@/components/map/Map";
 import Link from "next/link";
 import { CiShop } from "react-icons/ci";
 import { VscAccount } from "react-icons/vsc";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
-import { getArtistDetail,CleanArtist } from "@/app/redux/features/artists/artistActions";
-
+import {
+  getArtistDetail,
+  CleanArtist,
+} from "@/app/redux/features/artists/artistActions";
+import ReviewCard from "../../../components/reviewCard/ReviewCard";
 
 export default function Page({ params }) {
   // const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const dispatch = useDispatch()
-  const artist = useSelector((state) => state.artists.detail)
+  const dispatch = useDispatch();
+  const artist = useSelector((state) => state.artists.detail);
+  const [priceRanges, setPriceRanges] = useState({})
 
   useEffect(() => {
     if (params.id) {
       // setLoading(true);
-      dispatch(getArtistDetail(params.id))
+      dispatch(getArtistDetail(params.id));
     }
     return () => dispatch(CleanArtist());
   }, [params.id]);
 
   useEffect(() => {
-    if(artist.fullName){
+    if (artist.fullName) {
       // setLoading(false)
     }
-  }, [artist])
+    if(artist?.priceRanges?.length){
+      let obj = {}
+      artist.priceRanges.map((priceRange) => {
+        obj[priceRange.size] = {priceMin: priceRange.priceMin, priceMax: priceRange.priceMax}
+      })
+      setPriceRanges({...obj})
+    }
+  }, [artist]);
 
   // if (loading) return <div className="text-center">Cargando...</div>;
   if (error)
@@ -42,8 +53,6 @@ export default function Page({ params }) {
     );
   if (!artist)
     return <div className="text-center">No se encontró el tatuador</div>;
-
-   
 
   return (
     <div className="w-full p-4">
@@ -62,11 +71,11 @@ export default function Page({ params }) {
               className="w-48 h-48 object-cover rounded-full mx-auto"
             />
             <h2 className="items-center justify-center text-2xl font-bold mt-4 flex gap-2">
-            <VscAccount className="text-primary" /> {artist?.fullName}
+              <VscAccount className="text-primary" /> {artist?.fullName}
             </h2>
             <div className="mt-[10px]">
               <h2 className="text-2xl font-bold flex gap-2">
-              <CiShop className="text-primary" /> <p>{artist?.shopName}</p>
+                <CiShop className="text-primary" /> <p>{artist?.shopName}</p>
               </h2>
               <Link href={`/explore/${params.id}/reservas`}>
                 <button className="mt-[20px] ml-[50px] border-[1px] border-primary/75 text-primary/75 hover:border-primary hover:text-primary font-bold py-2 px-4 rounded">
@@ -74,21 +83,43 @@ export default function Page({ params }) {
                 </button>
               </Link>
             </div>
-            
+            {artist.reviews ? (
+              <div>
+                <h1>Reseñas dejadas por clientes sobre el artista: </h1>
+                {artist.reviews.map((review) => {
+                  return <ReviewCard 
+                    key={review.appointmentId}
+                    customerId={review.customerId}
+                    comment={review.comment}
+                    image={review?.image}
+                    rating={review.rating}
+                    />
+                })}
+              </div>
+            ) : (
+              <h1>El artista todavía no tiene reseñas, saca un turno y se el primero en dejar una!</h1>
+            )
+            }
           </div>
-
-          
         </div>
 
         <div className="w-full md:w-1/2 p-4 flex flex-col">
           <div className="p-4 rounded  bg-secondary-900 flex-grow">
-            <h3 className="text-xl font-bold  font-rocksalt flex gap-2 mb-4"> <FaMapLocationDot className="text-primary" /> Ubicacion</h3>
-            <MapComponent location={artist?.location} address={artist?.address} />
+            <h3 className="text-xl font-bold  font-rocksalt flex gap-2 mb-4">
+              {" "}
+              <FaMapLocationDot className="text-primary" /> Ubicacion
+            </h3>
+            <MapComponent
+              location={artist?.location}
+              address={artist?.address}
+            />
           </div>
 
           <div className="flex flex-wrap justify-around p-4 rounded mt-4  bg-secondary-900">
             <div>
-              <h3 className="text-xl font-bold mb-2 font-rocksalt">Estilos de tatuaje</h3>
+              <h3 className="text-xl font-bold mb-2 font-rocksalt">
+                Estilos de tatuaje
+              </h3>
               <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 {artist?.tattooStyles?.map((style, index) => (
                   <p key={index} className="flex items-center">
@@ -105,21 +136,23 @@ export default function Page({ params }) {
               <p>{artist?.phone}</p>
               <p>{artist?.address}</p>
               <p>{artist?.location}</p>
-              <p className="mb-1"><FaInstagram className="text-xl"/></p>
+              <p className="mb-1">
+                <FaInstagram className="text-xl" />
+              </p>
             </div>
 
             <div className="p-4 rounded ">
               <div className="flex flex-col items-center">
-                <label className="text-md  font-rocksalt">Pequeñoo</label>
-                <span className="text-gray-600">$</span>
+                <label className="text-md  font-rocksalt">Pequeño</label>
+                <span className="text-gray-600">$ {priceRanges.Pequeño?.priceMin} - {priceRanges.Pequeño?.priceMax}</span>
               </div>
               <div className="flex flex-col items-center mt-2">
-                <label className="text-md  font-rocksalt">Medianoo</label>
-                <span className="text-gray-600">$$</span>
+                <label className="text-md  font-rocksalt">Mediano</label>
+                <span className="text-gray-600">$ {priceRanges.Mediano?.priceMin} - {priceRanges.Mediano?.priceMax}</span>
               </div>
               <div className="flex flex-col items-center mt-2">
-                <label className="text-md  font-rocksalt">Grandee</label>
-                <span className="text-gray-600">$$$</span>
+                <label className="text-md  font-rocksalt">Grande</label>
+                <span className="text-gray-600">$ {priceRanges.Grande?.priceMin} - {priceRanges.Grande?.priceMax}</span>
               </div>
             </div>
           </div>
