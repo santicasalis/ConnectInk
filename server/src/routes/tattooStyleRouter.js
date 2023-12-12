d72681f5e7e0a23e44ef4609654a7aae6e49c749
santicasalis <santicasalis@hotmail.com>
2023-12-11 22:38:40 -0300

    Agrego el put a los Tattostyles

--- server/src/routes/tattooStyleRouter.js
+++ server/src/routes/tattooStyleRouter.js
@@ -4,9 +4,11 @@ const tattoStyleRouter = Router();
 const getTattoStylesHandler = require("../handlers/tattooStyleHandlers/getTattooStylesHandler");
 const createTattoStyleHandler = require("../handlers/tattooStyleHandlers/createTattooStyleHandler");
 const deleteTattoStyleHandler = require("../handlers/tattooStyleHandlers/deleteTattooStyleHandler");
+const updateTattoStylesHandler = require("../handlers/tattooStyleHandlers/updateTattoStylesHandler");
 
 tattoStyleRouter.get("/", getTattoStylesHandler);
 tattoStyleRouter.post("/", createTattoStyleHandler);
 tattoStyleRouter.delete("/:id", deleteTattoStyleHandler);
+tattoStyleRouter.put("/:id", updateTattoStylesHandler);
 
 module.exports = tattoStyleRouter;
